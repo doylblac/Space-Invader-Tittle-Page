2eef5b4aa78f6aea8e7a6c1cfd2b2a1ae64f8bf6
nathhood <nathhood287@ed.amdsb.ca>
2017-01-22 17:41:20 -0500

    I think I am done the tittle/highscore page

--- Space Invader Tittle Page/Form1.Designer.cs
+++ Space Invader Tittle Page/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playGameButton = new System.Windows.Forms.Button();
+            this.highScoreButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playGameButton
+            // 
+            this.playGameButton.BackColor = System.Drawing.Color.Black;
+            this.playGameButton.Font = new System.Drawing.Font("Visitor TT1 BRK", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playGameButton.Location = new System.Drawing.Point(237, 471);
+            this.playGameButton.Name = "playGameButton";
+            this.playGameButton.Size = new System.Drawing.Size(189, 46);
+            this.playGameButton.TabIndex = 0;
+            this.playGameButton.Text = "PLAY GAME";
+            this.playGameButton.UseVisualStyleBackColor = false;
+            this.playGameButton.Click += new System.EventHandler(this.playGameButton_Click);
+            // 
+            // highScoreButton
+            // 
+            this.highScoreButton.BackColor = System.Drawing.Color.Black;
+            this.highScoreButton.Font = new System.Drawing.Font("Visitor TT1 BRK", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highScoreButton.Location = new System.Drawing.Point(237, 523);
+            this.highScoreButton.Name = "highScoreButton";
+            this.highScoreButton.Size = new System.Drawing.Size(189, 45);
+            this.highScoreButton.TabIndex = 1;
+            this.highScoreButton.Text = "HIGH SCORES";
+            this.highScoreButton.UseVisualStyleBackColor = false;
+            this.highScoreButton.Click += new System.EventHandler(this.highScoreButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(766, 706);
+            this.ControlBox = false;
+            this.Controls.Add(this.highScoreButton);
+            this.Controls.Add(this.playGameButton);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button playGameButton;
+        private System.Windows.Forms.Button highScoreButton;
     }
 }
 
--- Space Invader Tittle Page/Form1.cs
+++ Space Invader Tittle Page/Form1.cs
@@ -12,9 +12,70 @@ namespace Space_Invader_Tittle_Page
 {
     public partial class Form1 : Form
     {
+        int page = 0;
+
+        List<int> highScores = new List<int>();
+
+        int score = 20001;
+
         public Form1()
         {
             InitializeComponent();
+
+            this.Size = new Size(500, 480);
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            highScores.Add(240);
+            highScores.Add(6310);
+            highScores.Add(5710);
+            highScores.Add(9750);
+            highScores.Add(4540);
+            highScores.Add(50975);
+            highScores.Add(45240);
+            highScores.Add(205);
+            highScores.Add(60);
+            highScores.Add(5870);
+            highScores.Add(6520);
+            highScores.Add(40);
+            highScores.Add(30);
+            highScores.Add(20);
+            highScores.Add(10);
         }
+
+        private void playGameButton_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.Black);
+            playGameButton.Visible = false;
+            highScoreButton.Visible = false;
+
+        }
+
+        private void highScoreButton_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Font bigFont = new Font("Consolas", 20, FontStyle.Bold); //create a font for graphics
+            Font smallFont = new Font("Consolas", 13, FontStyle.Bold); //create a font for graphics
+            SolidBrush greenBrush = new SolidBrush(Color.LimeGreen); //create a brush for graphics
+            SolidBrush whiteBrush = new SolidBrush(Color.White); //create a brush for graphics
+
+            highScoreButton.Visible = false;
+
+            g.Clear(Color.Black);
+
+            highScores.Add(score);
+            highScores.Sort();
+            highScores.Reverse();
+
+            int y = 80;
+
+            for (int i = 0; i < 10; i++)
+            {
+                g.DrawString("HIGH SCORES", bigFont, greenBrush, 150, 50);
+                g.DrawString("" + highScores[i], smallFont, whiteBrush, 150, y);
+                y = y + 20;
+            }
+        }
+
     }
 }
